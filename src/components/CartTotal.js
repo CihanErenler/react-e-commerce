5d943653b3ab44e3e187281c870d11cef2e1cff7
CihanErenler <cihanerenler@outlook.com>
2022-02-08 16:34:45 +0200

    delivery fee added

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -3,7 +3,7 @@ import { useCartContext } from "../context/CartContext";
 import { formatPrice } from "../common/helpers";
 
 const CartTotal = () => {
-  const { totalItems } = useCartContext();
+  const { totalItems, shippingFee } = useCartContext();
 
   return (
     <div className="sm:w-1/3 leading-10 border border-gray-300 p-4 mt-14 sm:ml-auto text-gray-600 mb-20 md:mb-0">
@@ -13,12 +13,12 @@ const CartTotal = () => {
       </div>
       <div className="flex items-center justify-between">
         <p>Shipping fee:</p>
-        <p>fee</p>
+        <p>{formatPrice(shippingFee)}</p>
       </div>
       <hr />
       <div className="flex items-center justify-between font-bold">
         <p>Order total:</p>
-        <p>{formatPrice(totalItems)}</p>
+        <p>{formatPrice(totalItems + shippingFee)}</p>
       </div>
     </div>
   );
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/hanci.svg";
 
 const Logo = () => {
   return (
     <div className="flex items-center">
       <Link to="/" className="text-5xl font-bold font-logo text-orange-300">
-        <span className="text-gray-500">han</span>
-        ci.
+        <img src={logo} alt="hanci logo" />
       </Link>
     </div>
   );
