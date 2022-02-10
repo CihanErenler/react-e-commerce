3d7d365a7a0e4e90ba38090dbb40ed2822b1a0fc
CihanErenler <cihanerenler@outlook.com>
2022-02-10 21:28:48 +0200

    credentials updated

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-4w4v012y.us.auth0.com"
-      clientId="GvvYwgzK5lpwql1a1I3SmwMdlJ3sLtBI"
+      domain="dev-udcr1gzo.us.auth0.com"
+      clientId="WvlrGYzsxpP7rZR4iiTK6lXkobF7Zn0A"
       redirectUri={window.location.origin}
       cacheLocation="localstorage"
     >
